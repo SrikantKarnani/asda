e9a26046b33465c53d97d95c1d62d91b86e1c82b
srikantkarnani15 <srikantdaphins@gmail.com>
2018-06-24 22:07:36 +0530

    Changed

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
+// const https = require('https');
+// const fs = require('fs');
+// const path = require('path');
 const app = express();
 app.set('view engine' , 'ejs');
 app.set('views',"./views");
 app.use(express.static('dist'))
 app.get('/',(req,res)=>res.render('home'));
-const httpsOptions = {
-    cert : fs.readFileSync(path.join(__dirname , 'server.crt')),
-    key : fs.readFileSync(path.join(__dirname , 'server.key'))
-}
-https.createServer(httpsOptions,app).listen(process.env.PORT || 3000,()=>console.log("Server Started"));
+// const httpsOptions = {
+//     cert : fs.readFileSync(path.join(__dirname , 'server.crt')),
+//     key : fs.readFileSync(path.join(__dirname , 'server.key'))
+// }
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+module.exports = app; 
