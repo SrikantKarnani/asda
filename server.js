df149ed69b564e91a249b214602b7a0cd46445e9
srikantkarnani15 <srikantdaphins@gmail.com>
2018-06-24 22:26:39 +0530

    Changed

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/',(req,res)=>res.render('home'));
 //     key : fs.readFileSync(path.join(__dirname , 'server.key'))
 // }
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+ ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 module.exports = app;
